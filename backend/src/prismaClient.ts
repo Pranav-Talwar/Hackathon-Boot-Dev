e82715d8ad29db32b4befc4500bad64cca9cb70d
user.email <talwarpranav929@gmail.com>
2025-07-26 11:46:43 -0600

    Fresh Start

--- backend/src/prismaClient.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
-
-export const getPrisma = (url: string) =>
-  new PrismaClient({ 
-    datasourceUrl: url 
-  }).$extends(withAccelerate())
